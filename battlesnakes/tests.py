dfdcd78967ad64b1e200cafbf6ff0a2c008a90e8
benkoklis-afk <benkoklis@gmail.com>
2025-11-21 20:07:10 +0200

    Add reformat

--- battlesnakes/tests.py
+++ battlesnakes/tests.py
@@ -139,7 +139,9 @@ class ChooseMoveTest(unittest.TestCase):
                 "height": 3,
                 "width": 3,
                 "food": [{"x": 2, "y": 2}],
-                "snakes": [{"id": "snake-123", "body": [{"x": 1, "y": 1}, {"x": 0, "y": 1}]}],
+                "snakes": [
+                    {"id": "snake-123", "body": [{"x": 1, "y": 1}, {"x": 0, "y": 1}]}
+                ],
             },
             "game": {"id": "game-1"},
             "turn": 0,
@@ -159,7 +161,12 @@ class ChooseMoveTest(unittest.TestCase):
                 "height": 2,
                 "width": 2,
                 "food": [],
-                "snakes": [{"id": "snake-123", "body": [{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 0}]}],
+                "snakes": [
+                    {
+                        "id": "snake-123",
+                        "body": [{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 0}],
+                    }
+                ],
             },
             "game": {"id": "game-1"},
             "turn": 0,
